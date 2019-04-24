4840154ec76b814c758dc1c7a46a2828e37989aa
redfenix <alexanderkiryshin@gmail.com>
2019-04-24 22:56:22 +0300

    initial commit

---
+++ Assets/Scripts/Buildings/GuardTower.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class GuardTower : GeneralBuilding
+{
+    public override TileBase GetTile()
+    {
+        return levelManager.GetTile(TileNames.GUARD_TOWER);
+    }
+    public override Sprite GetSprite()
+    {
+        return levelManager.GetSprite(TileNames.GUARD_TOWER);
+    }
+
+    public override float GetPrice()
+    {
+        return Prices.GUARD_TOWER_PRICE;
+    }
+
+    public override string GetDescription()
+    {
+        return "Защищает вас от врагов";
+    }
+
+    public override bool IsCanBeBuild(Vector2Int position)
+    {
+
+        if (!(levelManager.resourceTilemap.Resources.GetLength(0) > position.x) ||
+            !(levelManager.resourceTilemap.Resources.GetLength(1) > position.y))
+        {
+            return false;
+        }
+        if (levelManager.availibleBuildingTilemap.IsAvailibleBuilding[position.x, position.y] == true)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void OnBuilding()
+    {
+        
+    }
+
+    public override void OnEndTurn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Buildings/OilRig.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OilRig : GeneralBuilding
+{
+    public const float AMOUNT_OIL_PRODUCING = 10; 
+
+    public override TileBase GetTile()
+    {
+       return levelManager.GetTile(TileNames.OIL_RIG);
+    }
+    public override Sprite GetSprite()
+    {
+        return levelManager.GetSprite(TileNames.OIL_RIG);
+    }
+
+    public override float GetPrice()
+    {
+        return Prices.OIL_RIG_IN_OIL_PRICE;
+    }
+
+    public override string GetDescription()
+    {
+        return "Добывает "+AMOUNT_OIL_PRODUCING+"ед. нефти в ход";
+    }
+
+    public override bool IsCanBeBuild(Vector2Int position)
+    {   
+        
+        if (!(levelManager.resourceTilemap.Resources.GetLength(0) > position.x) ||
+            !(levelManager.resourceTilemap.Resources.GetLength(1) > position.y))
+        {
+            return false;        
+        }
+        if (levelManager.resourceTilemap.Resources[position.x, position.y] == Resource.Oil)
+        {
+            if (levelManager.availibleBuildingTilemap.IsAvailibleBuilding[position.x, position.y] == true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override void OnBuilding()
+    {
+        ModificatorManager.Instance.RegisterResourceModificator(new OilRigResModificator());
+        var modificator =(OilRigResModificator) ModificatorManager.Instance.GetResourceModificator(new OilRigResModificator());
+        resourceManager.IncomeOil +=modificator.GetOilIncome(AMOUNT_OIL_PRODUCING); 
+    }
+
+    public override void OnEndTurn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
